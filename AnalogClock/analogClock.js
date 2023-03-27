6c7f080e6611e23ff1a66e020bfb8bd6302647fb
bidanee <dleksql119@gmail.com>
2023-03-27 22:22:35 +0900

    analogClock original

--- AnalogClock/analogClock.js
+++ AnalogClock/analogClock.js
@@ -11,7 +11,7 @@ const AnalogClock = ($container) => {
     className: "hand second",
   });
   appendChildrenList($container,[$handHour,$handMinute,$handSecond]);
-
+  
   let time = null;
   for (let i = 1; i <= 12 ; i++) {
     time = Dom('div', {
@@ -20,6 +20,20 @@ const AnalogClock = ($container) => {
     })
     $container.append(time)
   }
+  
+  const setTime = () => {
+    const date = new Date();
+    let hours = date.getHours()
+    const minutes = date.getMinutes()
+    const seconds = date.getSeconds()
+    if(hours > 12) {hours = hours - 12}
+    $handHour.style.setProperty('--deg',hours * 30 + minutes * (30 / 60) + seconds * (0.5 / 60));
+    $handMinute.style.setProperty('--deg', minutes * 6 + seconds * (6 /60));
+    $handSecond.style.setProperty('--deg',seconds * 6)
+  }
+  
+  setTime()
+  setInterval(() => setTime(),1000)
 
 }
 
